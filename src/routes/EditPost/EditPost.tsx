d66d62676720403af3cbc5f87425b1cdf7fd9bf0
noahmifsud <noahmifsud2003@gmail.com>
2022-07-11 17:59:58 +1000

    Reworked tags input
    Updated the tag input to be more efficient with less useState hooks and more maintainable and readable code.

--- src/routes/EditPost/EditPost.tsx
+++ src/routes/EditPost/EditPost.tsx
@@ -3,41 +3,35 @@
 //React (Hooks) imports
 import React, { useState } from 'react'
 
+//React icons imports
+import { HiX } from 'react-icons/hi';
+
 
 function EditPost() {
-    //Post useState data
+    //useStates that store inputted data
     const [titleTxt, setTitleTxt] = useState('');
     const [descTxt, setDescTxt] = useState('');
-    const [tagCount, setTagCount] = useState(0);
-    const [showAddTags, setShowAddTags] = useState(true);
-    const [tagsStyle, setTagsStyle] = useState(['hidden', 'hidden', 'hidden'])
-    const [tag1Txt, setTag1Txt] = useState('');
-    const [tag2Txt, setTag2Txt] = useState('');
-    const [tag3Txt, setTag3Txt] = useState('');
+    const [tagTxt, setTagTxt] = useState('');
+    //useState stores all tags
+    const [tags, setTags] = useState(['']);
+
     //Handles input by updating useStates with user input
     const inputHandler = (e:any, setInput:any) => {
         setInput(e.target.value);
     }
-    //Adds new tags
-    const addTagHandler = () => {
-        if (tagCount < 3) {
-            //Get new tagCount value immediately
-            const tempTagCount = tagCount + 1;
-            switch(tempTagCount) {
-                case 1:
-                    setTagsStyle(['visible', 'hidden', 'hidden']);
-                    setTagCount(tagCount + 1);
-                    break;
-                case 2:
-                    setTagsStyle(['visible', 'visible', 'hidden']);
-                    setTagCount(tagCount + 1);
-                    break;
-                case 3: 
-                    setTagsStyle(['visible', 'visible', 'visible']);
-                    setShowAddTags(false);
-                    break;   
-                }
-        }
+    //Creates a tag and adds it to tags
+    const addTag = (e:any) => {
+        e.preventDefault();
+        let tempTags = [...tags];
+        tempTags.push(tagTxt);
+        setTags(tempTags);
+        setTagTxt('');
+    }
+    //Deletes a tag from tags
+    const deleteTag = (tagNum:number) => {
+        let tempTags = [...tags];
+        tempTags.splice(tagNum, 1);
+        setTags(tempTags);
     }
 
 
@@ -61,14 +55,25 @@ function EditPost() {
             </section>
             <section>
                 <h2>Tags -</h2>
-                <label htmlFor='tag1' className={tagsStyle[0]}>Tag 1:</label>
-                <input type='text' placeholder='Action' id='description' className={tagsStyle[0]} value={tag1Txt} onChange={(e) => inputHandler(e, setTag1Txt)}/>
-                <label htmlFor='tag2' className={tagsStyle[1]}>Tag 2:</label>
-                <input type='text' placeholder='Action' id='description' className={tagsStyle[1]} value={tag2Txt} onChange={(e) => inputHandler(e, setTag2Txt)}/>
-                <label htmlFor='tag3' className={tagsStyle[2]}>Tag 3:</label>
-                <input type='text' placeholder='Action' id='description' className={tagsStyle[2]} value={tag3Txt} onChange={(e) => inputHandler(e, setTag3Txt)}/>
-                {showAddTags &&
-                    <button className='add-tag-btn' onClick={addTagHandler}>ADD TAG</button>
+                {tags.map((tag, index) => {
+                    if (tag !== '') {
+                        return <div className='tag-div' key={index}>
+                                    <h3>{tag}</h3>
+                                    <HiX onClick={() => deleteTag(tags.indexOf(tag))}/>
+                                </div>
+                    } else {
+                        return <div key={index} className='hidden'></div>
+                    }
+                }
+                )}
+                {tags.length <= 3 &&
+                <div>
+                    <label htmlFor='tagInput' className=''>Enter Tag:</label>
+                    <form className='tag-input'>
+                        <input type='text' placeholder='Action' id='tagInput' className='' value={tagTxt} onChange={(e) => inputHandler(e, setTagTxt)}/>
+                        <button onClick={(e) => addTag(e)}>+</button>
+                    </form>
+                </div>
                 }
                 <p>Note: Max of 3 tags</p>
             </section>
@@ -78,9 +83,13 @@ function EditPost() {
                 <h1>{titleTxt}</h1>
                 <h2>{descTxt}</h2>
                 <div className='tags'>
-                    <p>{tag1Txt}</p>
-                    <p>{tag2Txt}</p>
-                    <p>{tag3Txt}</p>
+                    {tags.map((tag, index) => {
+                        if (tag !== '') {
+                            return <p key={index}>{tag}</p>
+                        } else {
+                            return <div key={index} className='hidden'></div>
+                        }
+                    })}
                 </div>
             </header>
         </article>
