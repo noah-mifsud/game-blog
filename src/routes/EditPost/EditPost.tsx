e4df095c6ee2b50944cbfa979c94d1051a9529c5
noahmifsud <noahmifsud2003@gmail.com>
2022-07-12 22:11:49 +1000

    Changed input method for editing post
    Imported react contenteditable to allow for users to edit data from the display rather than from the edit tools tab.

--- src/routes/EditPost/EditPost.tsx
+++ src/routes/EditPost/EditPost.tsx
@@ -1,97 +1,83 @@
-//Edit post route, displays post data with editing tools avaliable to the user
-
-//React (Hooks) imports
-import React, { useState } from 'react'
-
-//React icons imports
+/*  ROUTE
+    Displays posts and tools for user to edit a post
+*/
+//React imports
+import React, { useState } from 'react';
+import ContentEditable from 'react-contenteditable';
 import { HiX } from 'react-icons/hi';
 
 
 function EditPost() {
-    //useStates that store inputted data
-    const [titleTxt, setTitleTxt] = useState('');
-    const [descTxt, setDescTxt] = useState('');
-    const [tagTxt, setTagTxt] = useState('');
-    //useState stores all tags
-    const [tags, setTags] = useState(['']);
+    //Title states
+    const [titleText, setTitleText] = useState('Title');
+    const [taglineText, setTaglineText] = useState('Insert tagline');
+    const [taglineToggle, setTaglineToggle] = useState(false);
+    const [tagList, setTagList] = useState(['Action']);
 
-    //Handles input by updating useStates with user input
-    const inputHandler = (e:any, setInput:any) => {
-        setInput(e.target.value);
+    //Updates useState when called
+    const updateText = (e:any, setState:any) => {
+        setState((e.target.value));
     }
-    //Creates a tag and adds it to tags
-    const addTag = (e:any) => {
-        e.preventDefault();
-        let tempTags = [...tags];
-        tempTags.push(tagTxt);
-        setTags(tempTags);
-        setTagTxt('');
+    //Toggle element on page
+    const toggleElement = (setState:any, state:any) => {
+        setState(!state);
     }
-    //Deletes a tag from tags
-    const deleteTag = (tagNum:number) => {
-        let tempTags = [...tags];
-        tempTags.splice(tagNum, 1);
-        setTags(tempTags);
+    //Add new item to a list
+    const addToList = (setList:any, list:any, tempText:string) => {
+        let tempList = [...list];
+        tempList.push(tempText);
+        setList(tempList);
     }
-
-
+    const editList = (e:any, setList:any, list:any, index:number) => {
+        let tempList = [...list];
+        tempList.splice(index, 1, e.target.value);
+        setList(tempList);
+    }
+    const removeFromList = (setList:any, list:any, index:number) => {
+        let tempList = [...list];
+        tempList.splice(index, 1);
+        setList(tempList);
+    }
   return (
-    <main className='edit-post-page'>
+    <main>
+        {/* Edit Tools */}
         <aside className='edit-tools'>
-            <section className='header-edit-tools'>
-                <h2>Header Info -</h2>
-                <label htmlFor='title'>Title:</label><br></br>
-                <input type='text' placeholder='My review' id='title' value={titleTxt} onChange={(e) => inputHandler(e, setTitleTxt)}/>
-                <label htmlFor='description'>Description:</label>
-                <input type='text' placeholder='This is a game review' id='description' value={descTxt} onChange={(e) => inputHandler(e, setDescTxt)}/>
-            </section>
-            <section>
-                <h2>Media -</h2>
-                <div className='media-upload-btns'>
-                    <button>ADD IMAGE</button>
-                    <button>ADD VIDEO</button>
-                </div>
-                <p>Note: Only 1 image OR video can be added at a time</p>
-            </section>
-            <section>
-                <h2>Tags -</h2>
-                {tags.map((tag, index) => {
-                    if (tag !== '') {
-                        return <div className='tag-div' key={index}>
-                                    <h3>{tag}</h3>
-                                    <HiX onClick={() => deleteTag(tags.indexOf(tag))}/>
-                                </div>
+            <h2>Header -</h2>
+            {!taglineToggle ?
+                <button onClick={() => toggleElement(setTaglineToggle, taglineToggle)}>ADD TAGLINE</button> : <button className='remove-btn' onClick={() => toggleElement(setTaglineToggle, taglineToggle)}>REMOVE TAGLINE</button>
+            }
+            {tagList.map((item, index) => {
+                    if (item !== '') {
+                        return <div className='tags-edit'>
+                            <p dangerouslySetInnerHTML={{__html: item}}></p>
+                            <HiX className='delete-icon' onClick={() => removeFromList(setTagList, tagList, tagList.indexOf(item))}/>
+                        </div>
                     } else {
-                        return <div key={index} className='hidden'></div>
+                        return <div></div>
                     }
-                }
-                )}
-                {tags.length <= 3 &&
-                <div>
-                    <label htmlFor='tagInput' className=''>Enter Tag:</label>
-                    <form className='tag-input'>
-                        <input type='text' placeholder='Action' id='tagInput' className='' value={tagTxt} onChange={(e) => inputHandler(e, setTagTxt)}/>
-                        <button onClick={(e) => addTag(e)}>+</button>
-                    </form>
-                </div>
-                }
-                <p>Note: Max of 3 tags</p>
-            </section>
+            })
+            }
+            {tagList.length < 3 &&
+                <button onClick={() => addToList(setTagList, tagList, 'Action')}>ADD TAG</button>
+            }
+            <p className='note'>Note: Max of 3 tags per post.</p>
         </aside>
-        <article className='post-display'>
-            <header>
-                <h1>{titleTxt}</h1>
-                <h2>{descTxt}</h2>
-                <div className='tags'>
-                    {tags.map((tag, index) => {
-                        if (tag !== '') {
-                            return <p key={index}>{tag}</p>
-                        } else {
-                            return <div key={index} className='hidden'></div>
-                        }
-                    })}
-                </div>
-            </header>
+        {/* Post Document */}
+        <article className='edit-doc'>
+            <ContentEditable className='title' onChange={(e) => updateText(e, setTitleText)} html={titleText}/>
+            {taglineToggle &&
+                <ContentEditable className='tagline' onChange={(e) => updateText(e, setTaglineText)} html={taglineText}/>
+            }
+            <div className='tag-container'>
+                {tagList.map((item, index) => {
+                    if (item !== '') {
+                        return <ContentEditable key={index} className='tag' onChange={(e) => editList(e, setTagList, tagList, index)} html={tagList[tagList.indexOf(item)]}/>
+                    } else {
+                        return <div></div>
+                    }
+                })
+                }
+            </div>
         </article>
     </main>
   )
